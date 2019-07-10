0026ca02b9a73d6235d039f804b82d5c6e32fe5b
moonlagoon <courtneyroutsong@gmail.com>
2019-07-10 00:45:24 -0400

    Updates

---
+++ precache-manifest.c5edb24c2e853769e8339e32964b7b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "8c0b1743c9380274620c",
+    "url": "/clicky-game/static/js/main.cc3fb77b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8c0b1743c9380274620c",
+    "url": "/clicky-game/static/css/main.3fdbffe7.chunk.css"
+  },
+  {
+    "revision": "8fdfa049c8be354dc4b90abb3e859907",
+    "url": "/clicky-game/index.html"
+  }
+];
